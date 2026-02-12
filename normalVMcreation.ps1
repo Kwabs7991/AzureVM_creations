5a7608e3fa81d21b5ca706265a1e242d40ffcffe
Kwabs7991 <tabimikek@hotmail.co.uk>
2026-02-12 22:17:46 +0000

    Update normalVMcreation.ps1

--- normalVMcreation.ps1
+++ normalVMcreation.ps1
@@ -2,9 +2,13 @@
 
 New-AzVM `
 -ResourceGroupName "az104-rg8" ` 
--Name MyPSVM2 `
+-Name "MyPSVM2" `
 -Location "uksouth" `
 -Image "MicrosoftWindowsServer:WindowsServer:2025-datacenter-core-g2:latest" `
 -Zone "1" `
 -Size "Standard_D2lds_v6"
 -Credential (Get-Credential)
+
+---------------------------------------------------------------------------------------------------
+
+New-AzVM -ResourceGroupName "az104-rg8" -Name MyPSVM -Location "uksouth" -Image "MicrosoftWindowsServer:WindowsServer:2025-datacenter-core-g2:latest" -Zone "1" -Size "Standard_D2lds_v6"
